f2aa689211a5d7a3cc449c8d469552e766d4a080
rlippan <rlippan@50811bd7-b8ce-0310-adc1-d9db26280581>
2003-06-10 02:15:47 +0000

    Added nofoundrows to Makefile.PL. Updated dbdimp.c to Default to mysql_client_found_rows=1 Added note to install docs about compiling with Red Hat 8 || 9
    git-svn-id: https://svn.perl.org/modules/DBD-mysql/trunk@1121 50811bd7-b8ce-0310-adc1-d9db26280581

--- dbdimp.c
+++ dbdimp.c
@@ -847,10 +847,10 @@ MYSQL* mysql_dr_connect(MYSQL* sock, char* unixSocket, char* host,
 		  password ? password : "NULL");
   
   {
-#ifdef MYSQL_USE_CLIENT_FOUND_ROWS
-    unsigned int client_flag = CLIENT_FOUND_ROWS;
-#else
+#ifdef MYSQL_NO_CLIENT_FOUND_ROWS
     unsigned int client_flag = 0;
+#else
+    unsigned int client_flag = CLIENT_FOUND_ROWS;
 #endif
     mysql_init(sock);
     
