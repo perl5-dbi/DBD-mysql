0b8e0706f11f16a9258db01e18fddeab8ffcced8
capttofu <capttofu@50811bd7-b8ce-0310-adc1-d9db26280581>
2007-03-20 13:47:18 +0000

    * Fix to dbdimp.h do_error definition
    git-svn-id: https://svn.perl.org/modules/DBD-mysql/trunk@9284 50811bd7-b8ce-0310-adc1-d9db26280581

--- dbdimp.h
+++ dbdimp.h
@@ -284,11 +284,8 @@ struct imp_sth_st {
 #endif
 
 #include <dbd_xsh.h>
-#if MYSQL_VERSION_ID >= SQL_STATE_VERSION
 void    do_error (SV* h, int rc, const char *what, const char *sqlstate);
-#else
-void    do_error (SV* h, int rc, const char *what);
-#endif
+
 SV	*dbd_db_fieldlist (MYSQL_RES* res);
 
 void    dbd_preparse (imp_sth_t *imp_sth, SV *statement);
