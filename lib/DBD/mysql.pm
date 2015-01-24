6e65808d3a80621df0c8df3572e62db5ee1df1a2
Michiel Beijen <michielb@cpan.org>
2015-01-24 10:59:12 +0100

    Remove OLD installation instructions for win32
    It had still links to packages @ tcx.se.

--- lib/DBD/mysql.pm
+++ lib/DBD/mysql.pm
@@ -1905,9 +1905,6 @@ Here's an example of how to use the asynchronous query interface:
 
 =head1 INSTALLATION
 
-Windows users may skip this section and pass over to L<WIN32
-INSTALLATION> below. Others, go on reading.
-
 =head2 Environment Variables
 
 For ease of use, you can now set environment variables for
@@ -2008,83 +2005,6 @@ it:
   make
   make test
   make install
-
-=head1 WIN32 INSTALLATION
-
-If you are using ActivePerl, you may use ppm to install DBD-mysql.
-
-  ppm install DBI
-  ppm install DBD::mysql
-
-If you need an HTTP proxy, you might need to set the environment
-variable http_proxy, for example like this:
-
-  set http_proxy=http://myproxy.com:8080/
-
-
-I recommend using the win32clients package for installing DBD::mysql
-under Win32, available for download on www.tcx.se. The following steps
-have been required for me:
-
-=over
-
-=item -
-
-Extract sources into F<C:\>. This will create a directory F<C:\mysql>
-with subdirectories include and lib.
-
-IMPORTANT: Make sure this subdirectory is not shared by other TCX
-files! In particular do *not* store the MySQL server in the same
-directory. If the server is already installed in F<C:\mysql>,
-choose a location like F<C:\tmp>, extract the win32clients there.
-Note that you can remove this directory entirely once you have
-installed DBD::mysql.
-
-=item -
-
-Extract the DBD::mysql sources into another directory, for
-example F<C:\src\siteperl>
-
-=item -
-
-Open a CMD.exe shell and change directory to F<C:\src\siteperl>.
-
-=item -
-
-The next step is only required if you repeat building the modules: Make
-sure that you have a clean build tree by running
-
-  nmake realclean
-
-If you don't have VC++, replace nmake with your flavor of make. If
-error messages are reported in this step, you may safely ignore them.
-
-=item -
-
-Run
-
-  perl Makefile.PL
-
-which will prompt you for some settings. The really important ones are:
-
-  Which DBMS do you want to use?
-
-enter a 1 here (MySQL only), and
-
-  Where is your mysql installed? Please tell me the directory that
-  contains the subdir include.
-
-where you have to enter the win32clients directory, for example
-F<C:\mysql> or F<C:\tmp\mysql>.
-
-=item -
-
-Continued in the usual way:
-
-  nmake
-  nmake install
-
-=back
 
 =head1 AUTHORS
 
