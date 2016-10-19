1f8bb501a7791749aed166c9180ef8122127b1e7
Pali <pali@cpan.org>
2016-10-19 20:05:03 +0200

    Do not declare variable errno as it interact with one from errno.h
    Fixes windows warning: warning: '_errno' redeclared without dllimport
    attribute after being referenced with dll linkage [enabled by default].

--- dbdimp.c
+++ dbdimp.c
@@ -3258,7 +3258,6 @@ my_ulonglong mysql_st_internal_execute(
   char *table;
   char *salloc;
   int htype;
-  int errno;
 #if MYSQL_ASYNC
   bool async = FALSE;
 #endif
@@ -3417,7 +3416,7 @@ my_ulonglong mysql_st_internal_execute(
     do_error(h, mysql_errno(svsock), mysql_error(svsock), 
              mysql_sqlstate(svsock));
     if (DBIc_TRACE_LEVEL(imp_xxh) >= 2)
-      PerlIO_printf(DBIc_LOGPIO(imp_xxh), "IGNORING ERROR errno %d\n", errno);
+      PerlIO_printf(DBIc_LOGPIO(imp_xxh), "IGNORING ERROR errno %d\n", mysql_errno(svsock));
   }
   return(rows);
 }
