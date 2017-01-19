5efe2ba3c6133f1dddf6a5d57304fd8cbfa09e3c
Pali <pali@cpan.org>
2017-01-19 23:20:16 +0100

    Do not leak danging pointer to mysql result

--- dbdimp.c
+++ dbdimp.c
@@ -3297,7 +3297,10 @@ int dbd_st_more_results(SV* sth, imp_sth_t* imp_sth)
 
   /* Release previous MySQL result*/
   if (imp_sth->result)
+  {
     mysql_free_result(imp_sth->result);
+    imp_sth->result= NULL;
+  }
 
   if (DBIc_ACTIVE(imp_sth))
     DBIc_ACTIVE_off(imp_sth);
@@ -5550,6 +5553,7 @@ int mysql_db_async_result(SV* h, MYSQL_RES** resp)
       retval= mysql_num_rows(*resp);
       if(resp == &_res) {
         mysql_free_result(*resp);
+        *resp= NULL;
       }
     }
     if(htype == DBIt_ST) {
