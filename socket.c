531cdf416cbf4cca61d2f6c7dbdddd27c62e3efd
Pali <pali@cpan.org>
2017-01-29 02:53:49 +0100

    Implement select() based implementation of async mode
    select() is supported also by Windows and async mode is now enabled on them too.

--- socket.c
+++ socket.c
@@ -1,7 +1,12 @@
-#ifndef _WIN32
+#ifdef _WIN32
+#include "windows.h"
+#include "winsock.h"
+#endif
 
+#ifndef _WIN32
 #include <poll.h>
 #include <errno.h>
+#endif
 
 #include <mysql.h>
 
@@ -14,18 +19,36 @@
 int mysql_socket_ready(my_socket fd)
 {
   int retval;
+
+#ifdef _WIN32
+  /* Windows does not have poll(), so use select() instead */
+  struct timeval timeout;
+  fd_set fds;
+
+  FD_ZERO(&fds);
+  FD_SET(fd, &fds);
+
+  timeout.tv_sec = 0;
+  timeout.tv_usec = 0;
+
+  retval = select(fd+1, &fds, NULL, NULL, &timeout);
+#else
   struct pollfd fds;
 
   fds.fd = fd;
   fds.events = POLLIN;
 
   retval = poll(&fds, 1, 0);
+#endif
 
   if (retval < 0) {
+#ifdef _WIN32
+    /* Windows does not update errno */
+    return -WSAGetLastError();
+#else
     return -errno;
+#endif
   }
 
   return retval;
 }
-
-#endif
