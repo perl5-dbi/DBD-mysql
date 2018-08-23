20d3d99a39b788e2acfccc1cba4e050a128c381b
Daniël van Eeden <git@myname.nl>
2018-08-23 22:15:32 +0200

    Add options needed for public key based security.
    This is used for the sha256_password and the caching_sha2_password
    authntication plugins.

--- dbdimp.c
+++ dbdimp.c
@@ -1907,6 +1907,21 @@ MYSQL *mysql_dr_connect(
                          (SvTRUE(*svp) ? "utf8" : "latin1"));
         }
 
+#if (MYSQL_VERSION_ID >= 50723) && (MYSQL_VERSION_ID < MARIADB_BASE_VERSION)
+        if ((svp = hv_fetch(hv, "mysql_get_server_pubkey", 23, FALSE)) && *svp && SvTRUE(*svp)) {
+          my_bool server_get_pubkey = 1;
+          mysql_options(sock, MYSQL_OPT_GET_SERVER_PUBLIC_KEY, &server_get_pubkey);
+        }
+#endif
+
+#if (MYSQL_VERSION_ID >= 50600) && (MYSQL_VERSION_ID < MARIADB_BASE_VERSION)
+        if ((svp = hv_fetch(hv, "mysql_server_pubkey", 19, FALSE)) && *svp) {
+          STRLEN plen;
+          char *server_pubkey = SvPV(*svp, plen);
+          mysql_options(sock, MYSQL_SERVER_PUBLIC_KEY, server_pubkey);
+        }
+#endif
+
 	if ((svp = hv_fetch(hv, "mysql_ssl", 9, FALSE)) && *svp && SvTRUE(*svp))
           {
 	    my_bool ssl_enforce = 1;
